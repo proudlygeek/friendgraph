499e8ed68883e512657323139441d3328a661597
toretto460 <toretto460@gmail.com>
2012-09-02 19:40:10 +0200

    Friends of Friends

--- public/js/utils.js
+++ public/js/utils.js
@@ -1,22 +1,3 @@
-function switchBox(hiding, showing){
-    hiding.fadeOut('fast', function(){
-    	$(showing).fadeIn();
-    });
-}
-
-$(document).ready(function() {
-	$('.alert').fadeIn();
-	
-	$('#accept').click(function(){ 
-		switchBox($('.alert'), $('.btn-login'))
-	});
-
-	$('#deny').click(function(){
-		switchBox($('.alert'), $('#retry'));
-	});
-
-	$('#retry').click(function(){
-		switchBox($('#retry'), $('.alert'));
-	});
-	
-});	
+$(document).ready(function(){
+	init();
+});
